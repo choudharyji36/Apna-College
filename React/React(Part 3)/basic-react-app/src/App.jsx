61ec42614ab10f26f1eb26560c98833370420b4b
Kapil Choudhary <choudharyji36@gmail.com>
2025-07-24 21:48:45 +0530

    Code add

--- React/React(Part 3)/basic-react-app/src/App.jsx
+++ React/React(Part 3)/basic-react-app/src/App.jsx
@@ -6,11 +6,13 @@ import "./App.css";
 //import
 import ProductTab from  "./ProductTab.jsx";
 import Button from "./Button.jsx";
+import Form  from "./Form.jsx";
 
 function App() {
   return (  
     <>
-      <Button/>
+      {/* <Button/> */}
+      <Form/>
     </>
 );
 
